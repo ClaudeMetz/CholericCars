40bd236d6994b627c6f541bdddf5212f45e03da4
Claude Metz <claudearmetz@gmail.com>
2020-09-12 15:50:17 +0200

    Add volume setting

--- control.lua
+++ control.lua
@@ -1,6 +1,5 @@
 require("util")  -- core.lualib
 
-
 local function player_init()
     return {
         last_tick = 0,
@@ -10,7 +9,7 @@ end
 
 script.on_init(function()
     global.players = {}
-    
+
     for index, _ in pairs(game.players) do
         global.players[index] = player_init()
     end
@@ -29,15 +28,16 @@ function entity_damaged(event)
     local entity = event.entity
     local position = entity.position
     local driver = entity.get_driver()
-    
+    local volume = settings.global["cc_volume"].value / 100
+
     if driver == nil then  -- Rate limiting is not needed on driverless cars anyways
-        entity.surface.play_sound{path="cc_expletives", position=position}
+        entity.surface.play_sound{path="cc_expletives", position=position, volume_modifier=volume}
     else
         local player = global.players[driver.player.index]
         local tick = game.tick
 
         if (tick - player.last_tick) > 60 and util.distance(player.last_position, position) > 0.01 then
-            entity.surface.play_sound{path="cc_expletives", position=position}
+            entity.surface.play_sound{path="cc_expletives", position=position, volume_modifier=volume}
             player.last_tick = tick
         end
 
---
+++ settings.lua
@@ -0,0 +1,13 @@
+data:extend{
+    {
+        type = "int-setting",
+        name = "cc_volume",
+        setting_type = "runtime-global",
+        localised_name = "Volume",
+        localised_description = "Determines the volume as a percentage (0-100)",
+        default_value = 100,
+        minimum_value = 0,
+        maximum_value = 100,
+        order = "a"
+    }
+}
